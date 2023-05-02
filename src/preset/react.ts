cde2aea6a876f997f84d6d3ffc37da51b76b7d4c
MarvelSQ <sun1993hlj@hotmail.com>
2023-05-02 14:11:46 +0800

    fix(doc): 修复文档运行时报错

--- src/preset/react.ts
+++ src/preset/react.ts
@@ -2,12 +2,13 @@ import { createReplacer } from "../core";
 import { HookHelper } from "../helper/hook";
 import { SourceHelper } from "../helper/source";
 import { createMatcher } from "../matcher";
+import { SentenceType } from "../type";
 
 const dict = new Map();
 
 const replacer = createReplacer({
   matcher: createMatcher({
-    test: /[\u4e00-\u9fa5]/,
+    test: (value) => /[\u4e00-\u9fa5]/.test(value),
   }),
   assignee: {
     getLocaleKey(text) {
@@ -19,22 +20,62 @@ const replacer = createReplacer({
       return localeKey;
     },
   },
+  helpers: {
+    source: SourceHelper,
+    hook: HookHelper,
+  },
   plugins: [
-    HookHelper(
-      {
-        importSource: "./Intl/index",
-        name: "useIntl",
-        isDefault: false,
+    {
+      inject: [
+        {
+          type: "source",
+          option: {
+            importSource: "./Intl/index",
+            name: "useIntl",
+            isDefault: false,
+          },
+        },
+        {
+          type: "hook",
+          option: {
+            name: "{source.localImportName}",
+            result: "formatMessage",
+          },
+        },
+      ],
+      template: (context, sentence) => {
+        if (
+          [SentenceType.JSXText, SentenceType.JSXAttributeText].includes(
+            sentence.type as any
+          )
+        ) {
+          return '\\{{hook.hookResult}("{localeKey}"{parts?, {parts}})\\}';
+        }
+        return '{hook.hookResult}("{localeKey}"{parts?, {parts}})';
       },
-      {
-        result: "formatMessage",
-      }
-    ),
-    SourceHelper({
-      importSource: "./Intl/index",
-      name: "formatMessage",
-      isDefault: false,
-    }),
+    },
+    {
+      inject: [
+        {
+          type: "source",
+          option: {
+            importSource: "./Intl/index",
+            name: "formatMessage",
+            isDefault: false,
+          },
+        },
+      ],
+      template: (context, sentence) => {
+        if (
+          [SentenceType.JSXText, SentenceType.JSXAttributeText].includes(
+            sentence.type as any
+          )
+        ) {
+          return '\\{{source.localImportName}("{localeKey}"{parts?, {parts}})\\}';
+        }
+        return '{source.localImportName}("{localeKey}"{parts?, {parts}})';
+      },
+    },
   ],
 });
 
