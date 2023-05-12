b61ed65d768dea3ef7554bf35a942b67c4086f2a
MarvelSQ <sun1993hlj@hotmail.com>
2023-05-13 00:41:42 +0800

    fix: 修复构建缺失模块

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'node:path';
+import path from "node:path";
 
 export default defineConfig({
   root: __dirname,
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src/"),
+      "@": "/src",
     },
   },
   plugins: [
