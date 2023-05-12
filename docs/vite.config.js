98087e022cbfec796348f7cc173d190c81bb5901
MarvelSQ <sun1993hlj@hotmail.com>
2023-05-13 00:28:54 +0800

    fix: 修复alias解析

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'node:path';
 
 export default defineConfig({
   root: __dirname,
@@ -9,7 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "./src",
+      "@": path.resolve(__dirname, "src/"),
     },
   },
   plugins: [
