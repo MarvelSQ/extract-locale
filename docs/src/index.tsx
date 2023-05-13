3f5a5131ae6c78900f9887fe6bdc0348b289274f
MarvelSQ <sun1993hlj@hotmail.com>
2023-05-14 01:39:07 +0800

    fix: 修复重定向路径错误

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
       },
       {
         path: "task",
-        element: <Navigate to="task/detail" />,
+        element: <Navigate to="/task/detail" />,
       },
       {
         path: "task/:type",
