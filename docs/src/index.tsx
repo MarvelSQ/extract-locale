6abe756bebf6eca2a62912ba656c505fe8595674
MarvelSQ <sun1993hlj@hotmail.com>
2023-05-18 10:19:40 +0800

    build: 修复repo子path命中

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -6,22 +6,20 @@ import {
   RouterProvider,
   Navigate,
 } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import "./main.css";
 import { Home } from "./pages";
 import { Task } from "./pages/Task";
 import { Root } from "./pages/Root";
 import { ModalProvider } from "./lib/modal";
-import { getRepos, repoQueryClient } from "./filesystem/queries";
-
-const repos = getRepos();
+import { repoQueryClient } from "./filesystem/queries";
 
 const RedirectRoute = () => {
   const params = new URLSearchParams(window.location.search);
 
   const path = params.get("path");
 
-  return <Navigate to={path ? `/${path}` : "/"} />;
+  return <Navigate to={path ? `/repo/${path}` : "/"} />;
 };
 
 const router = createBrowserRouter([
